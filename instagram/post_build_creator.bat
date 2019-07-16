89893e4ad045706045da0c4c64887c60ff669353
ChernyshVova <chernyshvova@gmail.com>
2019-07-16 21:58:49 +0300

    added creating build folder

---
+++ instagram/post_build_creator.bat
@@ -0,0 +1,10 @@
+
+DEL "%~dp0\build" /F /Q /S
+mkdir "%~dp0\build"
+mkdir "%~dp0\build\bin"
+mkdir "%~dp0\build\avatars"
+mkdir "%~dp0\build\images"
+copy "%~dp0\instarm\instagram.db" build\instagram.db
+
+xcopy "%~dp0\instarm\bin\Release" "%~dp0\build\bin"
+
