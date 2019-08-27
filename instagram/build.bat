e68fc122048f00cf6e9567040e4645cf48e7d18c
ChernyshVova <chernyshvova@gmail.com>
2019-08-28 00:03:30 +0300

    added building for 19 and 18 vs version

--- instagram/build.bat
+++ instagram/build.bat
@@ -1,3 +1,52 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" instarm.sln property:Configuration=Release -verbosity:quiet
-call post_build_creator.bat
-pause
+
+@echo off
+set version_19=2019
+set version_17=2017
+set basePath_19=C:\Program Files (x86)\Microsoft Visual Studio\%version_19%\Community\MSBuild\
+set basePath_17=C:\Program Files (x86)\Microsoft Visual Studio\%version_17%\Community\MSBuild\
+
+IF exist "%basePath%Current\Bin\MSBuild.exe" (
+"%basePath%Current\Bin\MSBuild.exe" "%~dp0\instarm.sln" /property:Configuration=Release -verbosity:quiet
+)
+
+set basePath=C:\Program Files (x86)\Microsoft Visual Studio\%basePath_17%\Community\MSBuild\
+
+IF exist "%basePath%15.0\Bin\MSBuild.exe" (
+"%basePath%15.0\Bin\MSBuild.exe" "%~dp0\instarm.sln" property:Configuration=Release -verbosity:quiet
+)
+
+call "%~dp0\post_build_creator.bat" >nul
+
+@echo off
+
+
+                       
+echo   ^(                                 ^_
+echo   ^)                               ^/^=^>
+echo  ^(  ^+____________________/\/\___ / /^|
+echo   .''._____________'._____      / /^|/\
+echo  : () :              :\ ----\^|    \ )
+echo   '..'______________.'0^|----^|      \
+echo                    0_0/____/        \
+echo                        ^|----    /----\
+echo                       ^|^| -\\ --^|      \
+echo                       ^|^|   ^|^| ^|^|\      \
+echo                        \\____// '^|      \
+echo Bang! Bang!                    .'/       ^|
+echo                               .:/        ^|
+echo                               :/_________^|
+
+ping 127.0.0.1 -n 2 > nul
+color 4
+ping 127.0.0.1 -n 2 > nul
+color 7
+ping 127.0.0.1 -n 2 > nul
+color 4
+ping 127.0.0.1 -n 2 > nul
+color 7
+ping 127.0.0.1 -n 2 > nul
+color 4
+ping 127.0.0.1 -n 2 > nul
+color 7
+echo done!
+pause >nul
--- instagram/post_build_creator.bat
+++ instagram/post_build_creator.bat
@@ -1,9 +1,23 @@
 
+
 DEL "%~dp0\build" /F /Q /S
+
+IF Not exist "%~dp0\build"  (
 mkdir "%~dp0\build"
+)
+
+IF Not exist "%~dp0\build\bin" (
 mkdir "%~dp0\build\bin"
+)
+
+IF Not exist "%~dp0\build\avatars" (
 mkdir "%~dp0\build\avatars"
+)
+
+IF Not exist "%~dp0\build\images" (
 mkdir "%~dp0\build\images"
+)
+
 copy "%~dp0%instarm\bin\Release\instagram.db" build\instagram.db
 
 xcopy "%~dp0\instarm\bin\Release" "%~dp0\build\bin"
