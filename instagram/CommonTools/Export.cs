70195856a16cab6ff6747381c9c004d690525041
ChernyshVova <chernyshvova@gmail.com>
2019-09-14 16:36:37 +0300

    added test functionality for autotests from C# side

---
+++ instagram/CommonTools/Export.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+using mail;
+
+namespace CommonTools
+{
+        public class PyWrapper : DynamicObject
+        {
+
+            // wrapped C# class
+            private mail.MailAgent functions;
+
+            // ctor
+            public PyWrapper()
+            {
+                functions = new mail.MailAgent("chernyshvova@gmail.com", "BamBam753951");
+            }
+
+            public string ParseMessage()
+            {
+                DateTime messageTime = new DateTime(2000, 1, 1);
+                var code = functions.ParseMessage("Подтвердите свой аккаунт", "security@mail.instagram.com", messageTime, new InstagrammVerifyParser());
+                return code;
+            }
+
+        }
+}
